8a21d2524f7058e2d676e82d5e5b055511e76138
Your Name <e0323044@sriher.edu.in>
2026-06-03 09:39:54 +0530

    fix: update port and mongodb config

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db');
 const routes = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
 connectDB();
